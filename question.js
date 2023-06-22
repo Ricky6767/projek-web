5f1ccefa0b8bbe34d617549cf9b39e2f59985c0e
Ricky6767 <137399640+Ricky6767@users.noreply.github.com>
2023-06-22 18:08:40 +0700

    Add files via upload

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+{
+    numb: 1,
+    question: "Which one of these has a different meaning from the rest?",
+    answer: "C. Tiny",
+    options: [
+        "A. Huge",
+        "B. Gigantic",
+        "C. Tiny",
+        "D. Big"
+    ]
+},  
+{
+    numb: 2,
+    question: "Which of these is NOT related to taste?",
+    answer: "B. Humid",
+    options: [
+        "A. Sweet",
+        "B. Humid",
+        "C. Bitter",
+        "D. Sour"
+    ]
+}, 
+{
+    numb: 3,
+    question: "Which of these adjectives means 'dirty'?",
+    answer: "A. Filthy",
+    options: [
+        "A. Filthy",
+        "B. Awful",
+        "C. Starving",
+        "D. Furious"
+    ]
+}, 
+{
+    numb: 4,
+    question: "Which of these is NOT an adjective?",
+    answer: "C. Slowly",
+    options: [
+        "A. Careful",
+        "B. Long",
+        "C. Slowly",
+        "D. White"
+    ]
+}, 
+{
+    numb: 5,
+    question: "Which of these is NOT a Verb 2?",
+    answer: "C. Read",
+    options: [
+        "A. Went",
+        "B. Talked",
+        "C. Read",
+        "D. Caught"
+    ]
+}
+]
